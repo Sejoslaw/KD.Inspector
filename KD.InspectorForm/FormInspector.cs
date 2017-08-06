1fc17fa65baa11f478e66e70f521bb3a7f490b19
Krzysztof Dobrzynski <k.dobrzynski94@gmail.com>
2017-08-06 09:31:36 +0200

    More work on displaying

--- KD.InspectorForm/FormInspector.cs
+++ KD.InspectorForm/FormInspector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace KD.InspectorForm
@@ -17,6 +18,40 @@ namespace KD.InspectorForm
             {
                 string path = fileDialog.FileName;
                 L_FilePath.Text = path;
+
+                Assembly readedAssembly = Assembly.LoadFile(L_FilePath.Text);
+                Type[] types = readedAssembly.GetTypes();
+                foreach (Type type in types)
+                {
+                    TV_FileDetails.BeginUpdate();
+
+                    var firstLayer = TV_FileDetails.Nodes;
+                    var firstLayerCount = firstLayer.Count - 1;
+
+                    firstLayer.Add(type.Name);
+
+                    var secondLayer = firstLayer[firstLayerCount].Nodes;
+                    secondLayer.Add("Fields");
+                    foreach (FieldInfo field in type.GetFields())
+                    {
+                        var thirdLayer = secondLayer[secondLayer.Count - 1].Nodes;
+                        
+                        thirdLayer.Add(field.Name);
+                    }
+
+                    secondLayer.Add("Methods");
+                    foreach (MethodInfo method in type.GetMethods())
+                    {
+                        var thirdLayer = secondLayer[secondLayer.Count - 1].Nodes;
+                        string methodString = 
+                            (method.IsPublic ? "public " : "private ") + 
+                            (method.IsStatic ? "static " : "") + 
+                            (method.ReturnType.ToString() + " ") + 
+                            method.Name + "(" + 
+                    }
+
+                    TV_FileDetails.EndUpdate();
+                }
             }
         }
     }
